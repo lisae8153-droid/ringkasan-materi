c28ea6c5728d191174be0eded1188fc224a6d58f
lisae8153-droid <lisae8153@gmail.com>
2026-02-11 14:05:25 +0700

    Tambah app.py

---
+++ app.py
@@ -0,0 +1,33 @@
+import streamlit as st
+
+# Judul Web
+st.set_page_config(page_title="Asisten Pembuat Intisari Materi")
+
+st.title("📘 Asisten Pembuat Intisari Materi")
+st.write("Masukkan materi, lalu sistem akan membuat ringkasan dalam bentuk paragraf.")
+
+# Input teks
+teks = st.text_area("Masukkan Materi di Sini:", height=200)
+
+# Tombol Ringkas
+if st.button("Buat Ringkasan"):
+
+    if teks.strip() == "":
+        st.warning("⚠️ Materi tidak boleh kosong!")
+    else:
+
+        # Pisahkan jadi kalimat
+        kalimat = teks.split(".")
+
+        # Ambil maksimal 3 kalimat pertama
+        jumlah = min(3, len(kalimat))
+
+        ringkasan = ""
+
+        for i in range(jumlah):
+            if kalimat[i].strip() != "":
+                ringkasan += kalimat[i].strip() + ". "
+
+        # Output
+        st.subheader("Hasil Ringkasan:")
+        st.success(ringkasan)
